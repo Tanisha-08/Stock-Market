2f814f29cf3454bba702f23e56c7adfc620c1e27
Tanisha-08 <61088901+Tanisha-08@users.noreply.github.com>
2020-10-20 23:00:21 +0530

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,17 @@
+module.exports = {
+    google:{
+        clientID:'355677829095-03n2q346ruhpvvkampsfuih7uucus0se.apps.googleusercontent.com',
+        clientSecret:'7m7bj00OzJ_8xwhaMOYY8DtN'
+    },
+    mongodb:{
+        url:'mongodb://kaushik:kaushik8811@cluster0-shard-00-00.frcvg.mongodb.net:27017,cluster0-shard-00-01.frcvg.mongodb.net:27017,cluster0-shard-00-02.frcvg.mongodb.net:27017/test?ssl=true&replicaSet=atlas-l5m9e3-shard-0&authSource=admin&retryWrites=true&w=majority'
+    },
+    session:{
+        cookie:'kaushik'
+    },
+    yahoo:{
+        url:'https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes',
+        host:'apidojo-yahoo-finance-v1.p.rapidapi.com',
+        key:'2475c07362msh46c8c2a22af0146p175758jsndfcfb7d881d1'
+    }
+}
